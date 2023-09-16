fc136b28c2d0f8a6f8e7289b7f9aa67cd39df3d6
VEDANT SINGH <vedantsingh0731@gmail.com>
2023-09-16 16:57:02 +0530

    prediction edit

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
+import torch
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
 db=SQLAlchemy(app)
@@ -15,9 +17,33 @@ class Todo(db.Model):
     def __repr__(self):
         return self.id;
 
+with open("classes.txt","r") as file:
+    class_names = file.read()
+    class_names = eval(class_names)
+
+
+
+
+model = torch.load("Image_model.pt",map_location=torch.device('cpu'))
+model.eval()
+from Image.going_modular.going_modular.predictions import pred_and_plot_image
+
+# Setup custom image path
+custom_image_path = "PotatoHealthy1.JPG"
+actual_class=["Healty_potato"]
+
+
+
 @app.route('/',methods=['POST','GET'])
 def index():
-    return  render_template('index.html')
+    # Predict on custom image
+    x = pred_and_plot_image(model=model,
+                        image_path=custom_image_path,
+                        class_names=class_names)
+
+    return json.dumps({"predicted_class":x[0],"with_probabilty":x[1]})
+    
+    
 if __name__=="__main__":
     app.run(debug=True)
     
